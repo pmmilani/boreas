a07f22a86bfc4ed7b40f9d9960ac5624573ee9d7
Pedro M <pedrommilani@gmail.com>
2019-09-18 16:04:07 -0700

    v1.0, added temp folder in testing

--- test/example_usage.py
+++ test/example_usage.py
@@ -13,9 +13,9 @@ def main():
     # order, including testing to see whether we can load the default model
     printInfo()    
     
-    #--------------- This first part trains a model using the BR=2 dataset
+    #--------------- This part trains a model using the BR=2 dataset
     print("\n")
-    print("(1) -------------- Extracting features from BR=2 case")
+    print("-------------- Extracting features from BR=2 case")
     # This line produces a set of features/labels and saves it to disk with name 
     # "JICF_BR2_supercoarse_trainingdata.pckl (default name)"
     input_file_name = "JICF_BR2_supercoarse.plt" # name of input tecplot binary file
@@ -23,22 +23,23 @@ def main():
                             data_path = "temp/JICF_BR2_supercoarse_trainingdata.pckl",
                             deltaT0 = 1, # Tmax-Tmin = 1 in this case
                             use_default_var_names = True, # variable names are default
+                            use_default_derivative_names = True, # derivative names are default
                             calc_derivatives = True, # set to False if derivatives are already calculated in the file
-                            write_derivatives = False, # no need to cache derivatives 
+                            write_derivatives = False, # save tecplot file with derivatives if this is True 
                             model_type = "RF")   
     
     # Use the features extracted before to train the RF model
     print("\n")
-    print("(2) -------------- Training model on extracted features")
+    print("-------------- Training model on extracted features")
     feature_list = ["temp/JICF_BR2_supercoarse_trainingdata.pckl",]
     description = "Example RF model trained with supercoarse BR=2 LES"
     savepath = "temp/exampleRF.pckl"    
     trainRFModel(feature_list, description, savepath)
     
     
-    #------------- This second part applies the model trained previously to the BR=1 case
+    #------------- This part applies the model trained previously to the BR=1 case
     print("\n")
-    print("(3) -------------- Applying trained model on BR=1 case")
+    print("-------------- Applying trained RF model on BR=1 case")
     tecplot_file_name = "JICF_BR1_supercoarse.plt" # names of input tecplot binary file
     tecplot_file_output_name = "temp/JICF_BR1_supercoarse_out.plt" #output tecplot file
     fluent_interp_output_name = "temp/JICF_BR1_supercoarse_Prt.ip" #output Fluent ip file
@@ -46,13 +47,54 @@ def main():
     applyMLModel(tecplot_file_name, tecplot_file_output_name,
                  deltaT0 = 1, # Tmax-Tmin = 1 in this case
                  use_default_var_names = True, # variable names are default
+                 use_default_derivative_names = True, # derivative names are default
                  calc_derivatives = True, # set to False if derivatives are already calculated in the file
                  write_derivatives = False,
-                 ip_file_path=fluent_interp_output_name,
-                 csv_file_path=csv_output_name,
+                 ip_file_path = fluent_interp_output_name,
+                 csv_file_path = csv_output_name,
                  model_path = savepath, # path is the same we saved to previously
                  model_type = "RF") # here we choose the model type
     
-        
+    
+    
+    #------------- This part applies the model trained previously to the BR=1 case
+    print("\n")
+    print("-------------- Applying standard RF model on BR=1 case")
+    tecplot_file_name = "JICF_BR1_supercoarse.plt" # names of input tecplot binary file
+    tecplot_file_output_name = "temp/JICF_BR1_supercoarse_out.plt" #output tecplot file
+    fluent_interp_output_name = "temp/JICF_BR1_supercoarse_Prt.ip" #output Fluent ip file
+    csv_output_name = None # output csv file    
+    applyMLModel(tecplot_file_name, tecplot_file_output_name,
+                 deltaT0 = 1, # Tmax-Tmin = 1 in this case
+                 use_default_var_names = True, # variable names are default
+                 use_default_derivative_names = True, # derivative names are default
+                 calc_derivatives = True, # set to False if derivatives are already calculated in the file
+                 write_derivatives = False,
+                 ip_file_path = fluent_interp_output_name,
+                 csv_file_path = csv_output_name,
+                 model_path = None, # default model
+                 model_type = "RF") # here we choose the model type                
+                 
+    
+    #------------- This part applies the model trained previously to the BR=1 case
+    print("\n")
+    print("-------------- Applying standard anisotropic model on BR=1 case")
+    tecplot_file_name = "JICF_BR1_supercoarse.plt" # names of input tecplot binary file
+    tecplot_file_output_name = "temp/JICF_BR1_supercoarse_out_anisotropic.plt" #output tecplot file
+    fluent_interp_output_name = "temp/JICF_BR1_supercoarse_Aij.ip" #output Fluent ip file
+    csv_output_name = "temp/JICF_BR1_supercoarse_Aij.csv" # output csv file    
+    applyMLModel(tecplot_file_name, tecplot_file_output_name,
+                 deltaT0 = 1, # Tmax-Tmin = 1 in this case
+                 use_default_var_names = True, # variable names are default
+                 use_default_derivative_names = True, # derivative names are default
+                 calc_derivatives = True, # set to False if derivatives are already calculated in the file                 
+                 write_derivatives = False,
+                 ip_file_path = fluent_interp_output_name,
+                 csv_file_path = csv_output_name,
+                 model_path = None, # uses default model that comes with the package
+                 model_type = "TBNNS") # here we choose the model type    
+    
+    
+    
 if __name__ == "__main__":
     main()

515e43d80b71d4f1bd319bd098f62e78e397f516
Pedro M <pedrommilani@gmail.com>
2019-10-17 14:48:58 -0700

    Change constants for PRT_CAP=50

--- boreas/constants.py
+++ boreas/constants.py
@@ -22,7 +22,7 @@ TOL = 0.005
 MAX_CLEAN = 0.05
 
 # Cap for the value of Prt (symmetric, so the smallest value is 1.0/PRT_CAP)
-PRT_CAP = 20
+PRT_CAP = 50
 
 # Downsample, applied when producing the features/labels to be used at training time
 DOWNSAMPLE = 1.0
@@ -35,5 +35,5 @@ PROTOCOL = -1
 # Default parameters for training random forests
 N_TREES = 200 # number of trees to use in the random forest
 MAX_DEPTH = 20 # max depth of tree
-MIN_SPLIT = 0.001 # only split if the node has more than MIN_SPLIT * N points
+MIN_SPLIT = 0.0001 # only split if the node has more than MIN_SPLIT * N points
 N_PROCESSORS = -1 # how many processors used when training the ML algorithm.
